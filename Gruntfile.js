a0677aa969f1edffa56c6d7dd0a18023a9ed524c
Sebbor2 <sebbor2@gmail.com>
2017-12-13 10:30:34 +0100

    Configuration du projet morpion avec angularjs

---
+++ Gruntfile.js
@@ -0,0 +1,44 @@
+module.exports = function(grunt) {
+    // Définition de la configuration globale
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        // Config de chaque plugin
+        cssmin:{
+            app: {
+                files: {
+                    'dist/app.min.css': ['app.css']
+                }
+            }
+        },
+        concat: {
+            app: {
+                src: [
+                    'app/app.module.js',
+                    'app/*.component.js',
+                    'app/*.controller.js'
+                ],
+                dest: 'dist/app.js'
+            }
+        },
+        watch: {
+            cssSrc: {
+                files: ['**/*.css', '!node_modules/**'],
+                tasks: ['cssmin:app']
+            },
+            jsSrc: {
+                files: ['**/*.js'],
+                tasks: ['concat:app']
+            }
+        
+        }
+    });
+    
+    // Chargement des plugins.
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    
+    // Déclaration des taches exécutables.
+    grunt.registerTask('default',['cssmin:app', 'concat:app']);
+    grunt.registerTask('dev',['watch']);
+}
